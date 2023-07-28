c2dd83d489d0f0fac8a58c44f78d7d2f13ba0d0a
andersonbosa <andersonbosa0@gmail.com>
2023-07-27 21:46:55 -0300

    add rectangle

--- main.py
+++ main.py
@@ -11,20 +11,24 @@ def init_screen_capture():
     return screen_capture
 
 
-object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=128)
 
 
 # Object detection function
 def detect_objects(frame):
     mask = object_detector.apply(frame)
+
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
     for cnt in contours:
         area = cv2.contourArea(cnt)
         # if area < 16 and area > 4:
-        if area > 2**6:
-            cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 1)
+        if area > 2**7:
+            # cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 1)
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # cv2.imshow("Mask", mask)
+    cv2.imshow("Mask", mask)
     return frame
 
 
