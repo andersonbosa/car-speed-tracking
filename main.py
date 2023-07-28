ebadcfbfcb1bafcec436dc87a8a79c988942acdf
andersonbosa <andersonbosa0@gmail.com>
2023-07-27 21:36:32 -0300

    learn varThreshold

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import cv2
 import numpy as np
 
-# For YOLO object detection (if using YOLO)
-# from darknetpy.darknet import ObjectDetector
-
-# For TensorFlow object detection (if using TensorFlow)
-# from tensorflow_object_detector import ObjectDetector
-
 
 # Initialize the screen capture
 def init_screen_capture():
     # screen_capture = cv2.VideoCapture(0)  # Use 0 for the primary screen capture device
-    screen_capture = cv2.VideoCapture('/home/t4inha/devspace/the-cheat/_datasets/take4.mp4')
+    screen_capture = cv2.VideoCapture(
+        "/home/t4inha/devspace/the-cheat/_datasets/take4.mp4"
+    )
     return screen_capture
 
 
-object_detector = cv2.createBackgroundSubtractorMOG2()
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
 
 # Object detection function
@@ -24,7 +20,8 @@ def detect_objects(frame):
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area > 32 and area < 800:
+        # if area < 16 and area > 4:
+        if area > 2**6:
             cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 1)
 
     # cv2.imshow("Mask", mask)
@@ -36,7 +33,7 @@ def get_area_of_interest(frame):
     print(f"[DEBUG] x:{width}, y:{height}")
 
     y_1 = int(height / 4)
-    y_2 = y_1 + y_1 * 2
+    y_2 = y_1 * 3
 
     x_1 = 0
     x_2 = int(width)
